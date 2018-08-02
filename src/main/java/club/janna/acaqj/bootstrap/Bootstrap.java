b35b60d3ef623c0c0c2b1807813d4bd042983a0b
guopanbo <guopanbo@126.com>
2018-08-02 21:37:58 +0800

    引导器

---
+++ src/main/java/club/janna/acaqj/bootstrap/Bootstrap.java
@@ -0,0 +1,12 @@
+package club.janna.acaqj.bootstrap;
+
+/**
+ * @Auther: guopanbo
+ * @Date: 2018/8/2 21:03
+ * @Description:
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+
+    }
+}
