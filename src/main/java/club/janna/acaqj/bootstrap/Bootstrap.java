181e1ba82e375402fed2cffe2068055e98af4d1d
guopanbo <guopanbo@126.com>
2018-08-03 00:05:54 +0800

    引导器

--- src/main/java/club/janna/acaqj/bootstrap/Bootstrap.java
+++ src/main/java/club/janna/acaqj/bootstrap/Bootstrap.java
@@ -1,5 +1,11 @@
 package club.janna.acaqj.bootstrap;
 
+import club.janna.acaqj.collect.Collector;
+import club.janna.acaqj.collect.Executor;
+import club.janna.acaqj.model.Configure;
+import org.yaml.snakeyaml.Yaml;
+
+
 /**
  * @Auther: guopanbo
  * @Date: 2018/8/2 21:03
@@ -7,6 +13,11 @@ package club.janna.acaqj.bootstrap;
  */
 public class Bootstrap {
     public static void main(String[] args) {
-
+        Yaml yaml = new Yaml();
+        Configure configure = yaml.loadAs(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.yml"), Configure.class);
+        if(configure == null)
+            throw new RuntimeException("configure file load failed");
+        Executor.getInstance().execute(new Collector(configure.getRoot(), 1, null));
+//        Executor.getInstance().shutdown();
     }
 }
---
+++ src/main/java/club/janna/acaqj/model/Configure.java
@@ -0,0 +1,16 @@
+package club.janna.acaqj.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author guopanbo
+ * @Title: Configure
+ * @Description: TODO
+ * @date 2018/8/223:09
+ */
+@Getter
+@Setter
+public class Configure {
+    private String root;
+}
