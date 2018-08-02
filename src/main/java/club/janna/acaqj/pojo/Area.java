56c03dba8e5af932855fd4695b15cf770c1e15ca
guopanbo <guopanbo@126.com>
2018-08-02 22:54:16 +0800

    添加level字段

--- src/main/java/club/janna/acaqj/pojo/Area.java
+++ src/main/java/club/janna/acaqj/pojo/Area.java
@@ -3,6 +3,8 @@ package club.janna.acaqj.pojo;
 public class Area {
     private Integer id;
 
+    private Short level;
+
     private String code;
 
     private String name;
@@ -17,6 +19,14 @@ public class Area {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public Short getLevel() {
+        return level;
+    }
+
+    public void setLevel(Short level) {
+        this.level = level;
     }
 
     public String getCode() {
