885e6ec5abf22e375cdce4c8fd627a9f0f32672f
guopanbo <guopanbo@126.com>
2018-08-03 15:49:50 +0800

    加入3秒一次的线程池监控

--- src/main/java/club/janna/acaqj/bootstrap/Bootstrap.java
+++ src/main/java/club/janna/acaqj/bootstrap/Bootstrap.java
@@ -2,8 +2,11 @@ package club.janna.acaqj.bootstrap;
 
 import club.janna.acaqj.collect.Collector;
 import club.janna.acaqj.collect.Executor;
-import club.janna.acaqj.model.Configure;
-import org.yaml.snakeyaml.Yaml;
+import club.janna.acaqj.config.Configure;
+import club.janna.acaqj.util.ConfigureProvider;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ThreadPoolExecutor;
 
 
 /**
@@ -11,13 +14,20 @@ import org.yaml.snakeyaml.Yaml;
  * @Date: 2018/8/2 21:03
  * @Description:
  */
+@Slf4j
 public class Bootstrap {
     public static void main(String[] args) {
-        Yaml yaml = new Yaml();
-        Configure configure = yaml.loadAs(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.yml"), Configure.class);
-        if(configure == null)
-            throw new RuntimeException("configure file load failed");
+        Configure configure = ConfigureProvider.getConfigure();
         Executor.getInstance().execute(new Collector(configure.getRoot(), 1, null));
 //        Executor.getInstance().shutdown();
+        ThreadPoolExecutor executor = Executor.getInstance().getThreadPoolExecutor();
+        while(true) {
+            try {
+                Thread.sleep(3 * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info("queue size:{}, active count:{}, completed count:{}, task count:{}", executor.getQueue().size(), executor.getActiveCount(), executor.getCompletedTaskCount(), executor.getTaskCount());
+        }
     }
 }
