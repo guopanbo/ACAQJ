4b8c7a4163b9cc6b18ef087642670849cf664c7a
guopanbo <guopanbo@126.com>
2018-08-03 15:49:00 +0800

    configure provider

---
+++ src/main/java/club/janna/acaqj/util/ConfigureProvider.java
@@ -0,0 +1,26 @@
+package club.janna.acaqj.util;
+
+import club.janna.acaqj.config.Configure;
+import org.yaml.snakeyaml.Yaml;
+
+/**
+ * @author guopanbo
+ * @Title: ConfigureProvider
+ * @Description: TODO
+ * @date 2018/8/315:26
+ */
+public class ConfigureProvider {
+
+    private static Configure configure;
+
+    static {
+        Yaml yaml = new Yaml();
+        configure = yaml.loadAs(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.yml"), Configure.class);
+        if(configure == null)
+            throw new RuntimeException("configure file load failed");
+    }
+
+    public static Configure getConfigure() {
+        return configure;
+    }
+}
