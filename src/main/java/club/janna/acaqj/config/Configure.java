83c7d235f5b7d38320b4764ecb7af15955241fcf
guopanbo <guopanbo@126.com>
2018-08-04 11:32:59 +0800

    增加请求失败重试间隔时间和重试次数

--- src/main/java/club/janna/acaqj/config/Configure.java
+++ src/main/java/club/janna/acaqj/config/Configure.java
@@ -12,8 +12,29 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Configure {
+
+    /**
+     * 根节点
+     */
     private String root;
+
+    /**
+     * rabbit mq
+     */
     private RabbitMQ rabbitMQ;
+
+    /**
+     * 队列名称
+     */
     private String areaQueueName;
     private String errorLogQueueName;
+
+    /**
+     * 服务器不可用时重试间隔时间 （毫秒）
+     */
+    private Long tryAgainIntervalTime;
+    /**
+     * 服务器不可用时重试次数
+     */
+    private Integer tryAgainNum;
 }
